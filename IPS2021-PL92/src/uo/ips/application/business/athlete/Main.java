ffba81012e09d951147a73642a24018defec6472
Mister Rajoy <danielarezdiaz@gmail.com>
2021-10-10 20:43:20 +0200

    Test de la db

---
+++ IPS2021-PL92/src/uo/ips/application/business/athlete/Main.java
@@ -0,0 +1,21 @@
+package uo.ips.application.business.athlete;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import alb.util.jdbc.Jdbc;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection con = Jdbc.getConnection();
+		Statement st = con.createStatement();
+		
+		 boolean valid = con.isValid(50000);
+         System.out.println(valid ? "TEST OK" : "TEST FAIL");
+	}
+
+}
