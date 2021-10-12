3bf003e7f7e6200ad6d34f61b5eacf574467fa85
Mister Rajoy <danielarezdiaz@gmail.com>
2021-10-13 00:09:13 +0200

    Inscribir atleta

--- IPS2021-PL92/src/uo/ips/application/ui/inscribirUsuario.java
+++ IPS2021-PL92/src/uo/ips/application/ui/inscribirUsuario.java
@@ -7,9 +7,24 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import uo.ips.application.business.BusinessException;
+import uo.ips.application.business.BusinessFactory;
+import uo.ips.application.business.competicion.CompeticionDto;
+
+import java.awt.FlowLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.util.List;
+
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+
 public class inscribirUsuario extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +47,52 @@ public class inscribirUsuario extends JFrame {
 	 */
 	public inscribirUsuario() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 546, 300);
+		setBounds(100, 100, 741, 424);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(3, 1, 0, 0));
+		
+		JPanel panelEmail = new JPanel();
+		contentPane.add(panelEmail);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panelEmail.add(lblEmail);
+		
+		textField = new JTextField();
+		panelEmail.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panelCompeticion = new JPanel();
+		contentPane.add(panelCompeticion);
+		
+		JLabel lblCompeticion = new JLabel("Competicion");
+		lblCompeticion.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panelCompeticion.add(lblCompeticion);
+		
+		JComboBox<CompeticionDto> comboBoxCompeticiones = new JComboBox();
+		panelCompeticion.add(comboBoxCompeticiones);
+		
+		JPanel panelInscribirse = new JPanel();
+		contentPane.add(panelInscribirse);
+		
+		JButton btnInscribirse = new JButton("Inscribirse");
+		btnInscribirse.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		panelInscribirse.add(btnInscribirse);
+		
+		List<CompeticionDto> competiciones;
+		try {
+			competiciones = BusinessFactory.forCompeticionCrudService().ListarCompeticionesInscripcionesAbiertas();
+			
+			
+			for (CompeticionDto competicionDto : competiciones) {
+				comboBoxCompeticiones.addItem(competicionDto);			
+			}
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
