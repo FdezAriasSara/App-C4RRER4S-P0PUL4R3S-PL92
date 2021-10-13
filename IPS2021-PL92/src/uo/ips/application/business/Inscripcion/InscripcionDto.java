7c53a3f8cb898381852a2ada05c9cac3e3969c7b
UO276244 <UO276244@uniovi.es>
2021-10-13 20:13:55 +0200

    Añadida clase para crear Clasificaciones en funcion del sexo

--- IPS2021-PL92/src/uo/ips/application/business/Inscripcion/InscripcionDto.java
+++ IPS2021-PL92/src/uo/ips/application/business/Inscripcion/InscripcionDto.java
@@ -1,41 +1,50 @@
 package uo.ips.application.business.Inscripcion;
 
 import java.sql.Date;
+import java.sql.Time;
 
 public class InscripcionDto {
 
 	public static final int VACIO = -1;
 	public int idCompeticion;
-	public String idAtleta;	
-	public double cuota;
+	public int idAtleta;
 	public Date fechaInscripcion;
 	public Date fechaUltimoCambio;
 	public int idCategoria;
 	public int posicionFinal;
-	public float tiempoQueTarda;
+	public Time tiempoQueTarda;
+	public int tiempoQueTardaEnSegundos;
 	public Estado estado;
 
 	public enum Estado {
-		PRE_INSCRITO, INSCRITO, PENDIENTE
+		PRE_INSCRITO, INSCRITO, TERMINADA
 
 	};
 
 	
 
-	
-
-	public InscripcionDto(int idCompeticion, String idAtleta, double cuota, Date fechaInscripcion,
-			Date fechaUltimoCambio, int idCategoria, int posicionFinal, float tiempoQueTarda, Estado estado) {
+	@SuppressWarnings("deprecation")
+	public InscripcionDto(int idCompeticion, int idAtleta, String estado, Date fechaInscripcion,
+			Date fechaUltimoCambio, int idCategoria, int posicionFinal, Time tiempoQueTarda) {
 		
 		this.idCompeticion = idCompeticion;
 		this.idAtleta = idAtleta;
-		this.cuota = cuota;
+		//this.cuota = cuota;
 		this.fechaInscripcion = fechaInscripcion;
 		this.fechaUltimoCambio = fechaUltimoCambio;
 		this.idCategoria = idCategoria;
 		this.posicionFinal = posicionFinal;
 		this.tiempoQueTarda = tiempoQueTarda;
-		this.estado = estado;
+		this.tiempoQueTardaEnSegundos = tiempoQueTarda.getSeconds() +  tiempoQueTarda.getMinutes()*60 + tiempoQueTarda.getHours()*60*60;
+		
+		
+		
+		switch(estado) {
+		case("PRE_INSCRITO"): this.estado = Estado.PRE_INSCRITO; break;
+		case("INSCRITO"):  this.estado = Estado.INSCRITO; break;
+		case("TERMINADA"):  this.estado = Estado.TERMINADA; break;
+		}
+		
 	}
 
 
@@ -46,7 +55,7 @@ public class InscripcionDto {
 		this.fechaInscripcion = null;
 		this.fechaUltimoCambio = null;
 		this.posicionFinal = VACIO;
-		this.tiempoQueTarda = VACIO;
+		this.tiempoQueTardaEnSegundos = VACIO;
 	}
 
 }
