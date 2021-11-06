c93911866bc6d129aeacb26319c00a6ea6225831
UO276208 <73529340+UO276208@users.noreply.github.com>
2021-11-06 18:19:04 +0100

    actualizar pagos y inscripcion a partir del ficehro de pagos

--- IPS2021-PL92/src/uo/ips/application/business/pago/PagoDto.java
+++ IPS2021-PL92/src/uo/ips/application/business/pago/PagoDto.java
@@ -1,35 +1,45 @@
 package uo.ips.application.business.pago;
 
 import java.time.LocalDate;
-
+import java.util.UUID;
 
 public class PagoDto {
 
-	public LocalDate fechaPago;	
+	public LocalDate fechaPago;
 	public double importe;
 	public String pagoId;
 
-	//public String dniAtleta;
+	// public String dniAtleta;
 	public int idAtleta;
 	public int idCompeticion;
+	public double importe_devolver;
+
 	public PagoDto() {
-		
+
 	}
-	public PagoDto(LocalDate fechaPago,  int idAtleta, int idCompeticion) {
-		this.fechaPago = fechaPago;		
-	    this.idAtleta = idAtleta;
+
+	public PagoDto(LocalDate fechaPago, int idAtleta, int idCompeticion) {
+		this.fechaPago = fechaPago;
+		this.idAtleta = idAtleta;
 		this.idCompeticion = idCompeticion;
 	}
+
+	public PagoDto(LocalDate fechaPago, int idAtleta, int idCompeticion, double importe, double importe_devolver) {
+		this.fechaPago = fechaPago;
+		this.idAtleta = idAtleta;
+		this.idCompeticion = idCompeticion;
+		this.importe = importe;
+		this.importe_devolver = importe_devolver;
+		this.pagoId = UUID.randomUUID().toString();
+	}
+
 	/**
 	 * Para emitir justificantes de pago
 	 */
 	@Override
 	public String toString() {
-		return "fecha: " + fechaPago+"\n"+ " importe:" + importe +"$\n"+ " ID pago:" + pagoId +"\n"+  " ID atleta:"
-				+ idAtleta +"\n"+  " ID Competición:" + idCompeticion +"\n" ;
+		return "fecha: " + fechaPago + "\n" + " importe:" + importe + "$\n" + " ID pago:" + pagoId + "\n"
+				+ " ID atleta:" + idAtleta + "\n" + " ID Competición:" + idCompeticion + "\n";
 	}
 
-
-	
-	
 }
--- IPS2021-PL92/src/uo/ips/application/business/pago/crud/PagarConTransferencia.java
+++ IPS2021-PL92/src/uo/ips/application/business/pago/crud/PagarConTransferencia.java
@@ -11,22 +11,23 @@ import uo.ips.application.business.DtoAssembler;
 import uo.ips.application.business.competicion.CompeticionDto;
 
 public class PagarConTransferencia {
-	//private String REALIZAR_PAGO = "INSERT INTO Pago (idAtleta,idPago,fechaPago,importe) VALUES (?,?,?,?)";
+	// private String REALIZAR_PAGO = "INSERT INTO Pago
+	// (idAtleta,idPago,fechaPago,importe) VALUES (?,?,?,?)";
 	private String SQL_CAMBIAR_ESTADO_INSCRIPCION = "UPDATE Inscripcion SET estado='PENDIENTE DE PAGO' WHERE idAtleta=? and idCompeticion=?";
-	private String  SQL_BUSCAR_DATOS_TRANSFERENCIA = "SELECT * from Competicion where idCompeticion = ?";
+	private String SQL_BUSCAR_DATOS_TRANSFERENCIA = "SELECT * from Competicion where idCompeticion = ?";
 	private int idAtleta;
 	private int idCompeticon;
-	
+
 	public PagarConTransferencia(int idAtleta, int idCompeticion) {
 		this.idAtleta = idAtleta;
 		this.idCompeticon = idCompeticion;
 	}
-	
+
 	public CompeticionDto execute() throws BusinessException {
 		cambiarEstadoInscripcion();
 		return datos_Transferencia();
 	}
-	
+
 	private CompeticionDto datos_Transferencia() throws BusinessException {
 		Connection c = null;
 		PreparedStatement pst = null;
@@ -35,11 +36,10 @@ public class PagarConTransferencia {
 		try {
 			c = Jdbc.getConnection();
 
-			pst = c.prepareStatement( SQL_BUSCAR_DATOS_TRANSFERENCIA);
+			pst = c.prepareStatement(SQL_BUSCAR_DATOS_TRANSFERENCIA);
 			pst.setInt(1, idCompeticon);
 
-			competicion = DtoAssembler.toCompeticionDto(pst.executeQuery());
-			
+			competicion = DtoAssembler.toCompeticion(pst.executeQuery()).get();
 
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -50,23 +50,22 @@ public class PagarConTransferencia {
 
 		return competicion;
 	}
-	
+
 	private void cambiarEstadoInscripcion() throws BusinessException {
 		Connection c = null;
 		PreparedStatement pst = null;
 		ResultSet rs = null;
-		
+
 		try {
 			c = Jdbc.getConnection();
 
-			pst = c.prepareStatement( SQL_CAMBIAR_ESTADO_INSCRIPCION);
-			
+			pst = c.prepareStatement(SQL_CAMBIAR_ESTADO_INSCRIPCION);
+
 			pst.setInt(2, idCompeticon);
-			
-			pst.setInt(1, idAtleta);		
+
+			pst.setInt(1, idAtleta);
 			pst.executeUpdate();
-			
-				
+
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
