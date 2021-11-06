40588d007030903c0550a2ec89ec2fcd75769f54
UO276244 <UO276244@uniovi.es>
2021-11-06 11:29:10 +0100

    Añadidas librerias

--- IPS2021-PL92/src/gui/MainWindow.java
+++ IPS2021-PL92/src/gui/MainWindow.java
@@ -8,6 +8,10 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -23,10 +27,6 @@ import javax.swing.border.EmptyBorder;
 
 import com.toedter.calendar.JMonthChooser;
 import com.toedter.calendar.JYearChooser;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 
 public class MainWindow extends JFrame {
 
